71047a4901d8cca81a23433b6b449d795f9a3d2e
varNum462 <gary@varnumweb.com>
2022-03-04 20:53:03 -0500

    Created all transactions functions and menu.

--- atm.js
+++ atm.js
@@ -1,23 +1,39 @@
 "use strict";
-
-const account = require('./account.js');
-
+const prompt = require('prompt-sync')();
+const account = require('./account');
+const newTrans = require('./index');
+const currBalance = account.balance;
 //create functions getBalance, withdraw, deposit, validatePin
 
-function getBalance(){
+function getBalance(findBalance){
+    console.log(`Current Balance = ${findBalance.toFixed(2)}
+    
+    Would you like another transaction? Enter Yes or No`);
+    let newTrans = prompt();
+    if(newTrans === "Yes"){
+        
+    }else{ console.log(`Thank you for banking with Bank of Dev! Goodbye!`)}
+
 
 };
 
-function withdraw(){
-    
+function withdraw(wdAmt){
+    let newBalance = Number(currBalance) - Number(wdAmt);
+    return getBalance(newBalance);
 };
 
-function deposit(){
-    
+function deposit(depAmt){
+    let newBalance = Number(account.balance) + Number(depAmt);
+    console.log(`New Balance = ${newBalance.toFixed(2)}`);
+    return getBalance(newBalance);    
 };
 
-function validatePin(){
-    
+function validatePin(tryPIN){
+    if(tryPIN === account.pin){
+        return true;
+    }else{
+        return false;
+       }
 };
 
 module.exports.balance = getBalance;
--- index.js
+++ index.js
@@ -1,9 +1,59 @@
 "use strict";
 const prompt = require('prompt-sync')();
-const atm = require('./atm.js');
+const { balance } = require('./account');
+const atm = require('./atm');
 
 //As a developer, I want my index.js file to contain the user menu for interacting with the ATM.
 
 //As a developer, I want to account for and handle bad user input, ensuring that any user input is validated and reobtained if necessary.
 
 //Allow customer to continue using the ATM until choosing to exit.
+console.log(`Welcome to Bank of Dev! Please enter your 4-digit PIN:`);
+const getPIN = prompt();
+
+let ckPin = atm.valPIN(getPIN);
+if (ckPin === true){
+    console.log(`Thanks!`);
+    chooseTrans();
+}
+else{
+    console.log(`Ooops! Try again!`);
+}
+
+function chooseTrans(){
+    console.log(`Which transaction would you like to do?
+    For Balance Inquiry, enter 1
+    For Deposit, enter 2
+    For Withdrawal, enter 3
+    To exit, enter 4
+    `);
+    const transType = prompt();
+    switch(transType){
+        case '1':
+            atm.balance();
+        break;
+        case '2':
+            console.log(`How much would you like to deposit?`);
+            let depositAmt = prompt();
+            atm.deposit(depositAmt);
+        break;
+        case '3':
+            console.log(`How much would you like to withdraw?`);
+            let wdAmt = prompt();
+            if (wdAmt <= balance){
+                atm.withdraw(wdAmt);
+            }
+            else{
+                console.log(`You don't have sufficient balance for that withdrawal. Please enter an amount less than or equal to ${balance}.`);
+                let wdAmt = prompt();   
+                atm.withdraw(wdAmt);
+            }
+        break;
+        case '4':
+            console.log(`Thank you for banking with Bank of Dev! Goodbye!`)
+        default:
+            console.log(`Invalid Entry`);
+            chooseTrans();
+        break;
+    }
+}
